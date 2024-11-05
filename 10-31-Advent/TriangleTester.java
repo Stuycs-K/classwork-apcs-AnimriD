6aef386506b0c9665d4ea3f928ef97231769dd0c
Amrit Das <amritdas8015@gmail.com>
2024-11-05 10:59:36 -0500

    debugging

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,33 +1,66 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 public class TriangleTester {
-  public static void main(String[] args){
-    System.out.println(countTrianglesA("inputTri.txt"));
-  }
-  public static int countTrianglesA(String filename) {
-    //File file = new File(filename);
-    Scanner input = new Scanner (new File(filename));
-    int count = 0;
-    while (input.hasNextLine()) {
-      String line = input.nextLine();
-      String[]nums = line.split(" ");
-      for (int y = 0; y < nums.length - 2; y++) {
-        int a = Integer.parseInt(nums[y]);
-        int b = Integer.parseInt(nums[y+1]);
-        int c = Integer.parseInt(nums[y+2]);
-
-        if (validTri(Integer.parseInt(nums[y]), Integer.parseInt(nums[y] + 1), Integer.parseInt(nums[y] + 2))) {
-            count++;
+    public static int countTrianglesA(String filename) {
+        try {
+            int count = 0;
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            while (input.hasNextLine()) {
+                int s1 = input.nextInt();
+                int s2 = input.nextInt();
+                int s3 = input.nextInt();
+                if (validTri(s1, s2, s3)) {
+                    count++;
+                }
+            }
+            input.close();
+            return count;
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+            return 0;
         }
+
     }
-  }
-  input.close();
+  public static int countTrianglesB(String filename) {
+    int count = 0;
+    try {
+        File file = new File(filename);
+        Scanner input = new Scanner(file);
+        while (input.hasNextLine()) {
+            try {
+                int[] a = new int[3];
+                int[] b = new int[3];
+                int[] c = new int[3];
 
-  return count;
-}
+                for (int i = 0; i < 3; i++) {
+                    a[i] = input.nextInt();
+                    b[i] = input.nextInt();
+                    c[i] = input.nextInt();
+                }
 
+                for (int i = 0; i < 3; i++) {
+                    if (validTri(a[i], b[i], c[i])) {
+                        count++;
+                    }
+                }
+            } catch (NoSuchElementException ex) {
+                System.out.println("Incomplete line. Count " + count);
+                }
+            }
+            input.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+        return count;
+    }
   public static boolean validTri (int a, int b, int c) {
     return a + b > c && b + c > a && c + a > b;
+  }
+  public static void main(String[] args){
+        System.out.println(countTrianglesA("inputTri.txt"));
+        System.out.println(countTrianglesB("inputTri.txt"));
   }
 }
