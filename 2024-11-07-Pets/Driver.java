064200877728236caff5a0698e87d353f8de8ab3
Amrit Das <amritdas8015@gmail.com>
2024-11-07 10:47:18 -0500

    rewrote name and put comments in lines 1-3

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,6 @@
+// When calling the speak method on a Bird object
+// the words "The Mighty" was added before the name of the object
+// calling speak on animal objects remained the same with no "The Mighty".
 public class Driver{
   public static void main (String []args) {
     Animal a = new Animal("meow", 8, "kitty");
