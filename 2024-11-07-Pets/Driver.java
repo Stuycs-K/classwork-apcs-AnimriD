ac4d47b1a2d43c1aed67a98f7b90789f46b5ae58
Amrit Das <amritdas8015@gmail.com>
2024-11-07 10:38:32 -0500

    wrote bird class constructor and test case

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -2,5 +2,7 @@ public class Driver{
   public static void main (String []args) {
     Animal a = new Animal("meow", 8, "kitty");
     a.speak();
+    Bird birdie = new Bird("caw", 4, "bald eagle", 200, "brown");
+    birdie.speak();
   }
 }
