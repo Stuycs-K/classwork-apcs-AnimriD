d0e08f2aba648ed22b2f6cc5790d7f1dd22c51cb
Amrit Das <amritdas8015@gmail.com>
2024-11-20 10:59:42 -0500

    finished d2 part1

--- 2024-11-18-advent2016/d6.java
+++ 2024-11-18-advent2016/d6.java
@@ -9,7 +9,24 @@ public class d6{
             File file = new File(filename);
             Scanner input = new Scanner(file);
             String returnable = "";
-        }catch(FileNotFoundException ex){
+
+            ArrayList<String> columns;
+            columns = new ArrayList<String>();
+
+            while(input.hasNextLine()){
+              columns.add(input.nextLine());
+            }
+
+            ArrayList<String> alphabet;
+            alphabet = new ArrayList<String>();
+            for(int y = 0; y < 26 )
+
+            for(int x = 0; x < columns.size(); x++){
+
+            }
+        }
+
+        catch(FileNotFoundException ex){
             return "File not Found!";
         }
     }
