11f73548e6bcf12663ad69b8451a59d1bc733688
Amrit Das <amritdas8015@gmail.com>
2024-12-03 14:37:28 -0500

    added

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,10 @@
+public class ColorDemo{
+  public static void main(String[] args){
+    System.out.print("\u001b[2J");
+    System.out.print("\u001b[34m");
+    System.out.print("\u001b[41m");
+    System.out.print("\u001b[1m");
+    System.out.println("USA");
+    System.out.print("\u001b[0m");
+  }
+}
