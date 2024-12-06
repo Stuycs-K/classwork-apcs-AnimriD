09a41e4e00b713eaf8fd2bec380b694e3e94fa88
Amrit Das <amritdas8015@gmail.com>
2024-12-06 10:54:58 -0500

    wrote attack

--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public abstract class Warrior extends Adventurer{
   public Warrior(String name){
     super(name);
@@ -6,21 +7,30 @@ public abstract class Warrior extends Adventurer{
     super(name, hp);
   }
   private int swords;
-  public abstract String getSpecialName(){
+  public String getSpecialName(){
     return "swords";
   }
-  public abstract int getSpecial(){
+  public int getSpecial(){
     return this.swords;
   }
-  public abstract void setSpecial(int n){
+  public void setSpecial(int n){
     this.swords = n;
   }
-  public abstract int getSpecialMax(){
+  public int getSpecialMax(){
     return 1000;
   }
   public int restoreSpecial(int n){
     return super.restoreSpecial(n);
   }
-
+  public String attack(Adventurer other){
+    Random rand = Random();
+    int j = rand.nextInt(this.getSpecial() + 1);
+    other.HP = other.HP - j;
+    String returnable = "" + other.getName() + "was dealt " + j + "damage. ";
+    if(other.HP <= 0){
+      returnable += other.getName() + " is now dead."
+    }
+    return returnable;
+  }
 
 }
